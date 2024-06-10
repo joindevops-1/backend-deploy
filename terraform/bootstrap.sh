22baf5e9d10521739835c1488e27604f06b14c9c
joindevops <info@joindevops.com>
2024-06-10 21:03:21 +0530

    functions

---
+++ terraform/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+component=$1
+app_version=$2
+dnf install ansible -y
+pip3.9 install ansible botocore boto3
+ansible-pull -i localhost, -U https://github.com/joindevops-1/expense-ansible-roles-tf.git -e component=$component -e login_password=ExpenseApp1 main.yaml
